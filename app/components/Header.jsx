4a45d1253790a7823b5375431d8089d02b23978c
Nation47 <hananiyusuph98@gmail.com>
2023-09-15 21:03:02 -0700

    header

---
+++ app/components/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+    return (
+        <>
+            <header className="header">
+                <div className="nav">
+                    <div>
+                        <Link href='/'>Hannan's TechBlog</Link>
+                    </div>
+                    <div className="nav-list">
+                        <Link href='/'>home</Link>
+                        <Link href='/contact'>contact</Link>
+                        <Link href='/about'>about</Link>
+                        <Link href='/blog'>blog</Link>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+}
+ 
+export default Header;
